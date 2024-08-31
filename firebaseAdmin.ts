53c58e332dd5c06cf1c3976ecccb2c945e66e65c
gh0xsted <gh0xsted@gmail.com>
2024-08-31 01:00:00 -0700

    Fixed FBADmin

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,7 +1,13 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("@/service_key.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_KEY_BASE_64 as string
+);
+
+const serviceKey = JSON.parse(
+  Buffer.from(serviceAccount, "base64").toString("utf8")
+);
 
 let app: App;
 
